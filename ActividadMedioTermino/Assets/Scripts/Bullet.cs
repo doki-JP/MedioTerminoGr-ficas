45d211f6eb9207bef23eff9da1c8e5378764fc2a
Juan Pablo <a01705408@tec.mx>
2024-11-17 11:49:53 -0600

    Implementación solución fácil

--- ActividadMedioTermino/Assets/Scripts/Bullet.cs
+++ ActividadMedioTermino/Assets/Scripts/Bullet.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float bulletLife = 1f;  // Defines how long before the bullet is destroyed
-    public float rotation = 0f;
-    public float speed = 1f;
+    private ShootingManager manager;
 
-
-    private Vector2 spawnPoint;
-    private float timer = 0f;
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        spawnPoint = new Vector2(transform.position.x, transform.position.y);
+        // Encontrar el controlador central (asegúrate de que haya un ShootingManager en la escena)
+        manager = FindObjectOfType<ShootingManager>();
+
+        // Registrar la bala en el controlador
+        if (manager != null)
+        {
+            manager.RegistrarBala(gameObject);
+        }
     }
 
-
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        if (timer > bulletLife) Destroy(this.gameObject);
-        timer += Time.deltaTime;
-        transform.position = Movement(timer);
-    }
-
-
-    private Vector2 Movement(float timer)
-    {
-        // Moves right according to the bullet's rotation
-        float x = timer * speed * transform.right.x;
-        float y = timer * speed * transform.right.y;
-        return new Vector2(x + spawnPoint.x, y + spawnPoint.y);
+        // Eliminar la bala del controlador
+        if (manager != null)
+        {
+            manager.EliminarBala(gameObject);
+        }
     }
 
 }
--- ActividadMedioTermino/Assets/Scripts/EnemyShooting.cs
+++ ActividadMedioTermino/Assets/Scripts/EnemyShooting.cs
@@ -4,35 +4,20 @@ using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
 {
-    public GameObject proyectilPrefab; 
-    public Transform puntoDeDisparo;  
-    public float velocidadProyectil = 20f;
-    public float intervaloDisparoMin = 1f;
-    public float intervaloDisparoMax = 15f;
-    public float tiempoVidaProyectil = 15f;
+    public GameObject proyectilPrefab;
+    public Transform puntoDeDisparo;
+    public float velocidadProyectil = 50f;
+    public float tiempoVidaProyectil = 4f;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Disparar()
     {
-        Invoke("Disparar", Random.Range(intervaloDisparoMin,intervaloDisparoMax));
-
-    }
-
-    void Disparar()
-    {
-        // Instancia el proyectil en el punto de disparo y en la dirección de la torreta
+        // Crear el proyectil
         GameObject proyectil = Instantiate(proyectilPrefab, puntoDeDisparo.position, puntoDeDisparo.rotation);
-
-        // Asegúrate de que el proyectil tiene un Rigidbody para añadir velocidad
         Rigidbody rb = proyectil.GetComponent<Rigidbody>();
         if (rb != null)
         {
             rb.velocity = puntoDeDisparo.forward * velocidadProyectil;
         }
-
-        // Opcional: Destruir el proyectil después de unos segundos para optimizar memoria
         Destroy(proyectil, tiempoVidaProyectil);
-
-        Invoke("Disparar", Random.Range(intervaloDisparoMin, intervaloDisparoMax));
     }
 }
